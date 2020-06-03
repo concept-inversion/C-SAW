91b2e18cbe7664f0838592fd32872ba5fba3e89f
concept-inversion <santosh.pandey2222@gmail.com>
2020-06-02 22:44:10 -0400

    adds mpi version and renames sampler filename

--- non-stream/main.cu
+++ non-stream/main.cu
@@ -14,18 +14,26 @@
 #include <random>
 #include "api.cuh"
 #include "sampler.cuh"
+#include "sample_class.cuh"
 #include "functions.cuh"
 using namespace std;
 
-
 __global__ void
-check(Sampling *S, gpu_graph G,curandState *global_state,int n_subgraph, int FrontierSize, int NeighborSize)
+check(Sampling *S, gpu_graph G,curandState *global_state,int n_subgraph, int FrontierSize, int NeighborSize, int Depth)
 // check(graph,curandState *gs,sample,*neigh_l,n_blocks,*d_seed,n_threads,hash,bitmap)
 {
 	float prefix_time,local_d_time,global_d_time;
 	int tid = threadIdx.x + blockIdx.x * blockDim.x;
 	int hash=1, cache=0, bitflag=1, NORMALIZE=1;
-
+	#ifdef profile
+	if(tid==0){
+		printf("Sources.\n");
+		for(int i=0; i<n_subgraph;i++)
+		{
+			printf("%d\n",S->candidate.vertices[i]);
+		}}
+	#endif
+	__syncwarp();
 	int warpId = tid/32;
 	int warpTid=threadIdx.x%32;
     clock_t start_time,stop_time;
@@ -35,24 +43,27 @@ check(Sampling *S, gpu_graph G,curandState *global_state,int n_subgraph, int Fro
 	int __shared__ l_search[256];
 	int __shared__ max_find[256];
 	S->candidate.start[0] = 0;
-	int sourceIndex=0,source=0;
+	int sourceIndex=warpId,source=0;
 	if(warpTid==0){
-		sourceIndex=atomicAdd(&S->candidate.start[0],1);
+		atomicAdd(&S->candidate.start[0],1);
 	}
-	sourceIndex= __shfl_sync(0xffffffff,sourceIndex,0);
+	// sourceIndex= __shfl_sync(0xffffffff,sourceIndex,0);
 	__syncwarp();
 
+	#ifdef profile
+	if(threadIdx.x==0){printf("Block:%d, sourceIndex:%d,start: %d\n",blockIdx.x, sourceIndex, S->candidate.start[0]);}
+	#endif
 	// start loop
 	S->candidate.end[0]= n_subgraph;
 	// clock_t start = clock();
 	while(sourceIndex < S->candidate.end[0])
 	{
 		int VertCount=1;
-		source= S->candidate.instance_ID[sourceIndex];
+		source= S->candidate.vertices[sourceIndex];
 		int SampleID= S->candidate.instance_ID[sourceIndex];
 		int NL= G.degree_list[source];
 		#ifdef profile
-		// if(warpTid==0){printf("Source: %d, len: %d\n",source,NL);}
+		if(warpTid==0){printf(" Source: %d, len: %d\n",source,NL);}
 		#endif
 		if((NL==0) || (NL>8000)){	
 			if(warpTid==0){sourceIndex=atomicAdd(&S->candidate.start[0],1);}
@@ -68,7 +79,7 @@ check(Sampling *S, gpu_graph G,curandState *global_state,int n_subgraph, int Fro
 			else{
 				heur_normalize(&S->wvar[warpId],&S->cache,NeighborSize,1,local_state, &G,S->count.colcount, source, S->max,bitflag,cache);
 				}
-		frontier(&G,S,warpId,SampleID,NeighborSize,source,sourceIndex, hash);
+		frontier(&G,S,warpId,SampleID,NeighborSize,source,sourceIndex, hash, Depth);
 		
 		__syncwarp();
 		 if(warpTid==0){
@@ -77,19 +88,15 @@ check(Sampling *S, gpu_graph G,curandState *global_state,int n_subgraph, int Fro
 		sourceIndex= __shfl_sync(0xffffffff,sourceIndex,0);
 		__syncwarp();
 	}
-
-	if(tid==0){printf("Sampled Edges: %d,",S->count.counter[0]);}
+	if(tid==0){printf("%d,",S->sampled_count[0]);}
 }
 
 
-int main(int args, char **argv)
+struct arguments Sampler(char beg[100], char csr[100],int n_blocks, int n_threads, int n_subgraph, int FrontierSize, int NeighborSize, int Depth, struct arguments args, int rank)
+
 {
-	if(args!=9){std::cout<<"Wrong input\n"; return -1;}
-	int n_blocks= atoi(argv[4]);
-	int n_threads=atoi(argv[5]);
-	int n_subgraph=atoi(argv[6]);
-	int FrontierSize=atoi(argv[7]);
-	int NeighborSize=atoi(argv[8]);
+	// printf("A: %d\n",args);
+	// if(args!=9){std::cout<<"Wrong input\n"; return -1;
 	// cout<<"\nblocks:"<<n_blocks<<"\tThreads:"<<n_threads<<"\tSubgraphs:"<<n_subgraph<<"\n";
 	//int n_threads=32; 
 	
@@ -111,9 +118,9 @@ int main(int args, char **argv)
 	int total_mem_for_bitmap=n_blocks*PER_BLOCK_WARP*300;	
 	//std::cout<<"Input: ./exe beg csr nblocks nthreads\n";
 	
-	const char *beg_file=argv[2];
-	const char *csr_file=argv[3];
-	const char *weight_file=argv[3];
+	const char *beg_file=beg;
+	const char *csr_file=csr;
+	const char *weight_file=csr;
 	
 	graph<long, long, long, vertex_t, index_t, weight_t>
 	*ginst = new graph
@@ -124,7 +131,7 @@ int main(int args, char **argv)
 	cudaMalloc(&d_state,sizeof(curandState));  
 
 	Sampling *sampler;
-	Sampling S(ginst->edge_count, warps, 10000,n_subgraph, BUCKETS*BUCKET_SIZE, BUCKETS);
+	Sampling S(ginst->edge_count, warps, 10000,n_subgraph, BUCKETS*BUCKET_SIZE, BUCKETS, Depth);
 	H_ERR(cudaMalloc((void **)&sampler, sizeof(Sampling)));
 
 	// int *host_counter=(int *)malloc(sizeof(int));
@@ -134,7 +141,7 @@ int main(int args, char **argv)
 	
 	int *degree_list=(int *)malloc(sizeof(int)*ginst->edge_count);
 	std::random_device rd;
-    std::mt19937 gen(57);
+    std::mt19937 gen(56);
     std::uniform_int_distribution<> dis(1,10000);
 	int numBlocks;
 	//cudaGetDevice(&device);
@@ -156,14 +163,16 @@ int main(int args, char **argv)
 	int *d_seed;
 	int *d_total; 
 
-
+	int deviceCount;
+	HRR(cudaGetDeviceCount(&deviceCount));
+	// printf("My rank: %d, totaldevice: %d\n", rank,deviceCount);
+	// HRR(cudaSetDevice(rank%deviceCount));
 	int *bitmap, *node, *qstop_global, *qstart_global, *sample_id, *depth_tracker, *g_sub_index, *degree_l, *counter, *pre_counter;
 	int *nodes;
 
 	// HRR(cudaMalloc((void **) &hashtable,sizeof(int)*total_mem_for_hash));
 	HRR(cudaMalloc((void **) &bitmap,sizeof(int)*total_mem_for_bitmap)); 	
 	int *seeds=(int *)malloc(sizeof(int)*n_subgraph);
-	int *Samplecount=(int *)malloc(sizeof(int)*2);
 	int *h_sample_id=(int *)malloc(sizeof(int)*n_subgraph);
 	int *h_depth_tracker=(int *)malloc(sizeof(int)*n_subgraph);
 	
@@ -183,13 +192,21 @@ int main(int args, char **argv)
 	// shared variable for bincount and tempQ
 	double start_time,total_time;
 	start_time= wtime();
-	check<<<n_blocks, n_threads>>>(sampler, ggraph, d_state, n_subgraph, FrontierSize, NeighborSize);
+	check<<<n_blocks, n_threads>>>(sampler, ggraph, d_state, n_subgraph, FrontierSize, NeighborSize, Depth);
 	HRR(cudaDeviceSynchronize());
-	total_time= wtime()-start_time;
-	// HRR(cudaMemcpy(Samplecount,sampler->count.counter,sizeof(int)*2, cudaMemcpyDeviceToHost));
-	float rate = (float)(Samplecount[0]/total_time)/1000000;
-	printf("%s,SamplingTime:%.6f\n",argv[1],total_time);
-	// Copy the sampled graph to CPU
+	// HRR(cudaMemcpy(host_counter, sampler->sampled_count, sizeof(int), cudaMemcpyDeviceToHost));	
 	
-
+	// int total_count=0;
+	// for(int i=0; i < n_subgraph;i++){
+	// 	int count= S.samples[i].start[0];
+	// printf("Sampled: %d\n",host_counter[0]);
+	// 	total_count+=count;
+	// }
+	total_time= wtime()-start_time;
+	// printf("%s,SamplingTime:%.6f\n",argv[1],total_time);
+	// Copy the sampled graph to CPU
+	// printf("Sampled edges:%d\n",host_counter[0]);	
+	// args.sampled_edges=host_counter[0];
+	args.time=total_time; 
+	return args;
 }
---
+++ non-stream/mpi_main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <mpi.h>
+#include "sampler.cuh"
+#include <stdlib.h>
+#include <assert.h>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <netdb.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    // SampleSize, FrontierSize, NeighborSize
+    // printf("MPI started\n");
+    int n_blocks = atoi(argv[4]);
+    int n_threads = atoi(argv[5]);
+    int SampleSize = atoi(argv[6]);
+    int FrontierSize = atoi(argv[7]);
+    int NeighborSize = atoi(argv[8]);
+    int total_GPU = atoi(argv[9]);
+    MPI_Status status;
+    int myrank;
+    double global_max_time, global_min_time;
+    int global_sampled_edges;
+    struct arguments args;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank); 
+    int global_sum;
+    SampleSize = SampleSize/total_GPU;
+    
+    args= Sampler(argv[2],argv[3], n_blocks, n_threads, SampleSize, FrontierSize, NeighborSize,args,myrank);
+    MPI_Reduce(&args.time, &global_max_time, 1, MPI_DOUBLE,MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&args.time, &global_min_time, 1, MPI_DOUBLE,MPI_MIN, 0, MPI_COMM_WORLD);
+    float rate = global_sampled_edges/global_max_time/1000000;
+    if(myrank==0)
+    {
+        printf("%s,%f,%f,%f\n",argv[1],global_min_time,global_max_time,rate);
+    }
+    MPI_Finalize();
+   return 0;
+}
