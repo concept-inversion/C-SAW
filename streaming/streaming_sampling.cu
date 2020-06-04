0c8de571b9ce8ca3a1d955a945572b570d7e4b36
concept-inversion <santosh.pandey2222@gmail.com>
2020-06-03 21:21:03 -0400

    updates streaming

--- streaming/streaming_sampling.cu
+++ streaming/streaming_sampling.cu
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "herror.h"
+#include "sampler.cuh"
 #include <math.h>
 #include <curand.h>
 #include <curand_kernel.h>
@@ -256,7 +257,8 @@ check(int Graph_block_size,int streamid,int block_id,vertex_t* adj_list, index_t
 	int queue_start_address = block_id*queue_size;
     curandState local_state=global_state[threadIdx.x];
 	curand_init(tid, 0, 0, &local_state); // sequence created with different seed and same sequence
-	if(tid==0) {printf("Stream:%d, Graph_block:%d, Qstart:%d, Qstop:%d \n",streamid,block_id,qstart_global[block_id],qstop_global[block_id]);}
+
+	// if((threadIdx.x==0)&(blockIdx.x==0)) {printf("Stream:%d, Graph_block:%d, Qstart:%d, Qstop:%d \n",streamid,block_id,qstart_global[block_id],qstop_global[block_id]);}
 	int depth_flag=0;
 	edges_traversed = 0; 
 	// add all items to the combined queue: Number of threads must be greater than samples
@@ -404,7 +406,7 @@ check(int Graph_block_size,int streamid,int block_id,vertex_t* adj_list, index_t
 			__syncwarp();	
 		}
 	}	
-	if((threadIdx.x==0)&(blockIdx.x==0)) {printf("Going out: Stream:%d, Block:%d, Queue stop:%d, Queue start:%d \n",streamid,block_id,qstop_global[block_id],qstart_global[block_id]);}
+	// if((threadIdx.x==0)&(blockIdx.x==0)) {printf("Going out: Stream:%d, Block:%d, Queue stop:%d, Queue start:%d \n",streamid,block_id,qstop_global[block_id],qstart_global[block_id]);}
 	//qstart_global[block_id]=temp_queue_stop; // This line may require some barrier. Or we could use different variable for qstart  
 	
 }
@@ -447,13 +449,11 @@ int block_augument(int blocks, int vertex_count,index_t* beg_pos,int *beg_size_l
 	}
 }
 
-int main(int args, char **argv)
+struct arguments Sampler(char beg[100], char csr[100],int n_blocks, int n_threads, int n_subgraph, int frontier_size, int neighbor_size, struct arguments args, int rank)
 {
-	if(args!=7){std::cout<<"Wrong input\n"; return -1;}
-	int n_blocks= atoi(argv[4]);
-	int n_threads=atoi(argv[5]);
-	int n_subgraph=atoi(argv[6]);
-	cout<<"\nblocks:"<<n_blocks<<"\tThreads:"<<n_threads<<"\tSubgraphs:"<<n_subgraph<<"\n";
+	// if(args!=7){std::cout<<"Wrong input\n"; return -1;}
+
+	// cout<<"\nblocks:"<<n_blocks<<"\tThreads:"<<n_threads<<"\tSubgraphs:"<<n_subgraph<<"\n";
 	//int n_threads=32; 
 	int *total=(int *)malloc(sizeof(int)*n_subgraph);
 	int len=5;
@@ -473,9 +473,9 @@ int main(int args, char **argv)
 	int total_queue_memory = queue_size*Graph_block*n_subgraph;
 
 	//std::cout<<"Input: ./exe beg csr nblocks nthreads\n";
-	const char *beg_file=argv[2];
-	const char *csr_file=argv[3];
-	const char *weight_file=argv[3]; // unnecessary
+	const char *beg_file=beg;
+	const char *csr_file=csr;
+	const char *weight_file=csr; // unnecessary
 	//template <file_vertex_t, file_index_t, file_weight_t
 	//new_vertex_t, new_index_t, new_weight_t>
 	graph<long, long, long, vertex_t, index_t, weight_t>
@@ -486,6 +486,11 @@ int main(int args, char **argv)
 	int edge_count=ginst->edge_count;
 	int Graph_block_size=vertex_count/Graph_block;
 	// int Graph_block_size=2000;
+	printf("Size of blocks\n");
+	for(int i=0;i<4;i++)
+	{
+		printf("%d,%d\n",i,ginst->beg_pos[(i+1)*Graph_block_size]-ginst->beg_pos[(i)*Graph_block_size]);
+	}
 	curandState *d_state;
 	cudaMalloc(&d_state,sizeof(curandState));    
 	gpu_graph ggraph(ginst);
@@ -513,6 +518,12 @@ int main(int args, char **argv)
         check,
         n_threads,
 		0);
+
+	
+	int deviceCount;
+	HRR(cudaGetDeviceCount(&deviceCount));
+	// printf("My rank: %d, totaldevice: %d\n", rank,deviceCount);
+	// HRR(cudaSetDevice(rank%deviceCount));
 	// cout<<"Max allocatable Blocks:"<<numBlocks<<"\n";
 	int *d_node_list;
     int *d_edge_list;
@@ -604,21 +615,23 @@ int main(int args, char **argv)
 	double time_start=wtime();
 	while(sampling_complete==false)
 	{
-		display(block_active,Graph_block);
-		
+
+		// display(block_active,Graph_block);
+		if(1){
 		H_ERR(cudaMemcpyAsync(&ggraph.adj_list[adj_size_list[block_id1]],&ginst->adj_list[adj_size_list[block_id1]],adj_size_list[block_id2]-adj_size_list[block_id1]
 			, cudaMemcpyHostToDevice,stream1));
 		H_ERR(cudaMemcpyAsync(&ggraph.beg_pos[beg_size_list[block_id1]],&ginst->beg_pos[beg_size_list[block_id1]],beg_size_list[block_id2]-beg_size_list[block_id1]
 			, cudaMemcpyHostToDevice,stream1));
-		check<<<n_blocks/2,n_threads,0,stream1>>>(Graph_block_size,0,block_id1,ggraph.adj_list, ggraph.beg_pos, ggraph.weight_list, ggraph.vert_count,d_state,d_node_list,d_edge_list,d_neigh_l,d_degree_l,n_blocks,d_seed,n_threads,d_total,hashtable,bitmap,n_subgraph,node,queue,sample_id,depth_tracker,qstart_global,qstop_global,g_sub_index);
-		
+
+		check<<<n_blocks,n_threads,0,stream1>>>(Graph_block_size,0,block_id1,ggraph.adj_list, ggraph.beg_pos, ggraph.weight_list, ggraph.vert_count,d_state,d_node_list,d_edge_list,d_neigh_l,d_degree_l,n_blocks,d_seed,n_threads,d_total,hashtable,bitmap,n_subgraph,node,queue,sample_id,depth_tracker,qstart_global,qstop_global,g_sub_index);
+		}
 
 		if(block_active[1]){
 			H_ERR(cudaMemcpyAsync(&ggraph.adj_list[adj_size_list[block_id2]],&ginst->adj_list[adj_size_list[block_id2]],adj_size_list[block_id3]-adj_size_list[block_id2]
 				, cudaMemcpyHostToDevice,stream2));
 			H_ERR(cudaMemcpyAsync(&ggraph.beg_pos[beg_size_list[block_id2]],&ginst->beg_pos[beg_size_list[block_id2]],beg_size_list[block_id3]-beg_size_list[block_id2]
 				, cudaMemcpyHostToDevice,stream2));	
-		check<<<n_blocks/2,n_threads,1,stream2>>>(Graph_block_size,1,block_id2,ggraph.adj_list, ggraph.beg_pos, ggraph.weight_list, ggraph.vert_count,d_state,d_node_list,d_edge_list,d_neigh_l,d_degree_l,n_blocks,d_seed,n_threads,d_total,hashtable,bitmap,n_subgraph,node,queue,sample_id,depth_tracker,qstart_global,qstop_global,g_sub_index);
+		check<<<n_blocks,n_threads,1,stream2>>>(Graph_block_size,1,block_id2,ggraph.adj_list, ggraph.beg_pos, ggraph.weight_list, ggraph.vert_count,d_state,d_node_list,d_edge_list,d_neigh_l,d_degree_l,n_blocks,d_seed,n_threads,d_total,hashtable,bitmap,n_subgraph,node,queue,sample_id,depth_tracker,qstart_global,qstop_global,g_sub_index);
 		}
 
 		if(block_active[2]){
@@ -626,7 +639,7 @@ int main(int args, char **argv)
 				, cudaMemcpyHostToDevice,stream3));
 			H_ERR(cudaMemcpyAsync(&ggraph.beg_pos[beg_size_list[block_id3]],&ginst->beg_pos[beg_size_list[block_id3]],beg_size_list[block_id4]-beg_size_list[block_id3]
 				, cudaMemcpyHostToDevice,stream3));
-		check<<<n_blocks/2,n_threads,2,stream3>>>(Graph_block_size,2,block_id3,ggraph.adj_list, ggraph.beg_pos, ggraph.weight_list, ggraph.vert_count,d_state,d_node_list,d_edge_list,d_neigh_l,d_degree_l,n_blocks,d_seed,n_threads,d_total,hashtable,bitmap,n_subgraph,node,queue,sample_id,depth_tracker,qstart_global,qstop_global,g_sub_index);	
+		check<<<n_blocks,n_threads,2,stream3>>>(Graph_block_size,2,block_id3,ggraph.adj_list, ggraph.beg_pos, ggraph.weight_list, ggraph.vert_count,d_state,d_node_list,d_edge_list,d_neigh_l,d_degree_l,n_blocks,d_seed,n_threads,d_total,hashtable,bitmap,n_subgraph,node,queue,sample_id,depth_tracker,qstart_global,qstop_global,g_sub_index);	
 		}
 
 		if(block_active[3]){
@@ -634,7 +647,7 @@ int main(int args, char **argv)
 				, cudaMemcpyHostToDevice,stream4));
 			H_ERR(cudaMemcpyAsync(&ggraph.beg_pos[beg_size_list[block_id4]],&ginst->beg_pos[beg_size_list[block_id4]],beg_size_list[4]-beg_size_list[block_id4]
 				, cudaMemcpyHostToDevice,stream4));
-		check<<<n_blocks/2,n_threads,3,stream4>>>(Graph_block_size,3,block_id4,ggraph.adj_list, ggraph.beg_pos, ggraph.weight_list, ggraph.vert_count,d_state,d_node_list,d_edge_list,d_neigh_l,d_degree_l,n_blocks,d_seed,n_threads,d_total,hashtable,bitmap,n_subgraph,node,queue,sample_id,depth_tracker,qstart_global,qstop_global,g_sub_index);
+		check<<<n_blocks,n_threads,3,stream4>>>(Graph_block_size,3,block_id4,ggraph.adj_list, ggraph.beg_pos, ggraph.weight_list, ggraph.vert_count,d_state,d_node_list,d_edge_list,d_neigh_l,d_degree_l,n_blocks,d_seed,n_threads,d_total,hashtable,bitmap,n_subgraph,node,queue,sample_id,depth_tracker,qstart_global,qstop_global,g_sub_index);
 		}
 		//wait for completion
 		//find new top 3 blocks
@@ -663,8 +676,12 @@ int main(int args, char **argv)
 		printf("Value of i:%d\n",i);
 		
 		
-		if(q_count==0){printf("Sampling complete;\n");sampling_complete=true;}
-		if(i==10){printf("Sampling complete;\n");sampling_complete=true;}
+		if(q_count==0)
+		{//printf("Sampling complete;\n");
+		sampling_complete=true;}
+		if(i==10){
+			//printf("Sampling complete;\n");
+			sampling_complete=true;}
 		// printf("Total: %d,max:%d, value:%d, Value of i: %d\n",q_count,max,value,i);
 	}
 	HRR(cudaDeviceSynchronize());
@@ -672,9 +689,11 @@ int main(int args, char **argv)
 	HRR(cudaMemcpy(total,g_sub_index,sizeof(int)*n_subgraph, cudaMemcpyDeviceToHost));
 	int counted=sum(n_subgraph,total);
 	float rate = (float)(counted/cmp_time)/1000000;
-	printf("%s,Kernel time:%f, Rate (Million sampled edges): %f\n",argv[1],cmp_time,rate);
+	// printf("%s,Kernel time:%f, Rate (Million sampled edges): %f\n",argv[1],cmp_time,rate);
 	// printf("%s,Samples: %d, time:%f\n",argv[1],n_subgraph,cmp_time);
-	cudaDeviceReset();
+	args.sampled_edges=counted;
+	args.time=cmp_time; 
+	return args;
 }
 
 // void blocks_allocator(int n_blocks,int *Block,  )
